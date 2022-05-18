f010c36cb9f4f32d6ae82938c0c96f717b8199a6
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-05-18 16:07:56 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -5,37 +5,35 @@ document.addEventListener("DOMContentLoaded", () => {
     divContainer.className = "grid-container";
     let divHeader = document.createElement('div');
     divHeader.className = "header";
+    let h1Header = document.querySelector("h1");
     let pHeader = document.createElement("p");
     pHeader.innerText = "Take a sample from more than 490,000 works, throughout the world and history, and and let yourself be carried away by the feelings they inspire"
-    divHeader.appendChild(document.querySelector("h1"));
-    divHeader.appendChild(pHeader);
+    divHeader.append(h1Header, pHeader);
     let divSelector = document.createElement('div');
     divSelector.className = "scrollselector";
-    let pSelector = document.createElement("p");
-    pSelector.textContent = "Please Select Collection to See Sample";
+    let h2Selector = document.createElement("h2");
+    h2Selector.textContent = "Please Select Collection to See Sample";
     let divScrollMenu = document.createElement('div');
     divScrollMenu.className = "scrollmenu";
-    divSelector.appendChild(pSelector);
-    divSelector.appendChild(divScrollMenu);
+    divSelector.append(h2Selector, divScrollMenu)
     let imageContainer = document.querySelector("#object-image-container");
     imageContainer.className = "row";
     let divFooter = document.createElement('div');
     divFooter.className = "footer";
+    let h2Footer = document.createElement("h2");
+    h2Footer.textContent = "The Met Locations";
+    let divScrollFooter = document.createElement('div');
+    divScrollFooter.className = "scrollmenu";
     let firstFooterElement = document.createElement('a');
     firstFooterElement.className = "footer-item";
-    firstFooterElement.href = "#";
     firstFooterElement.innerText = "The Met Fifth Avenue";
     let secondFooterElement = document.createElement('a');
     secondFooterElement.className = "footer-item";
-    secondFooterElement.href = "#";
     secondFooterElement.innerText = "The Met Cloisters";
-    divFooter.appendChild(firstFooterElement);
-    divFooter.appendChild(secondFooterElement);
+    divScrollFooter.append(firstFooterElement, secondFooterElement);
+    divFooter.append(h2Footer, divScrollFooter);
     let bodyElement = document.querySelector("body");
-    divContainer.appendChild(divHeader);
-    divContainer.appendChild(divSelector);
-    divContainer.appendChild(imageContainer);
-    divContainer.appendChild(divFooter);
+    divContainer.append(divHeader, divSelector, imageContainer, divFooter);
     bodyElement.appendChild(divContainer);
   }
   createHtmlBase();
@@ -47,7 +45,6 @@ document.addEventListener("DOMContentLoaded", () => {
       let selectorElement = document.querySelector('.scrollmenu');
       let optionElement = document.createElement('a');
       optionElement.className = "scrollmenu-item";
-      optionElement.href = "#";
       optionElement.innerText = department["displayName"];
       optionElement.id = department["departmentId"];
       optionElement.addEventListener("click", optionCb)
@@ -92,11 +89,51 @@ document.addEventListener("DOMContentLoaded", () => {
     renderImagesByDepartment(selection);
   }
 
-  // function getValueSelector() {
-  //   let optionSelected = document.querySelector("#selector");
-  //   optionSelected.addEventListener("click", optionCb)
-  // }
+  function createFooterModal() {
+    const footerOptions = document.querySelectorAll(".footer-item");
+    // get Met Location onclick
+    footerOptions.forEach((footerOption) => {
+      footerOption.addEventListener("click", (event) => {
+            let metLocation = event.target.innerText.split(" ").join("");
+            console.log(metLocation);
+            //run modal function
+            footerModal(metLocation);
+        });
+    });
+  }
+    //creating the modal
+  function footerModal (location) {
+    const modal = document.createElement("div");
+    modal.className = "modal";
+    //add the modal to the main section or the parent element
+    document.querySelector(".footer").append(modal);
+    const modalContent = document.createElement("div");
+    modalContent.className = "modal-content";
+    //creating the close button
+    const closeButton = document.createElement("span");
+    closeButton.className = "close-button";
+    closeButton.innerText = "x";
+    closeButton.addEventListener("click", closeModal);
+    //adding image to modal
+    const locationImage = document.createElement("img");
+    locationImage.src = `./images/${location}.jpg`;
+    const locationMet = document.createElement("b");
+    locationMet.innerText = (location === "TheMetCloisters") ? "The Met Cloisters" : "The Met Fifth Avenue";
+    const locationAddress = document.createElement("p");
+    const addressFifthAve = "1000 Fifth Avenue\nNew York, NY 10028\nPhone: 212-535-7710";
+    const addressCloisters = "99 Margaret Corbin Drive\nFort Tryon Park\nNew York, NY 10040\nPhone: 212-923-3700";
+    locationAddress.innerText = (location === "TheMetCloisters") ? addressCloisters : addressFifthAve;
+    modalContent.append(closeButton, locationImage, locationMet, locationAddress);
+    modal.appendChild(modalContent);
+    modal.classList.toggle("show-modal");
+  }    
+  
+  function closeModal() {
+    const modal = document.querySelector(".modal");
+    console.log(modal);
+    modal.remove();
+  }
 
   renderImagesByDepartment(departmentId = "1");
-  // getValueSelector();
+  createFooterModal();
 })
